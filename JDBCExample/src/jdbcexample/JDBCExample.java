33f945fd636522fa9a2ff4063b82770984994517
Anjila Tripathi <anjila.tripathi@gmail.com>
2024-01-04 18:02:45 +0545

    Database Connectivity

--- JDBCExample/src/jdbcexample/JDBCExample.java
+++ JDBCExample/src/jdbcexample/JDBCExample.java
@@ -1,13 +1,16 @@
+import java.sql.*;
 
 public class JDBCExample {
 
-    public static void main(String[] args)  throws Exception
-    {
-
-        Class.forName("com.mysql.cj.jdbc.Drivers"); //load and register Driver
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost=3306", "root", "");
-        Statement st = conn.getStatement();
-        Resultset rs = st.executeUpdate();
-        conn.close;
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver"); //load and register Driver
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/database", "root", "");
+            Statement st = conn.createStatement();
+            String qry = "Create table information(id int(15) primary key auto_increment, `name` varchar(20), email  varchar (30))";
+            int rs = st.executeUpdate(qry);
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
     }
 }
