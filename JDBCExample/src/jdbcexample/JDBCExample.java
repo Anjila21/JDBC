459bef1716d984d97b0d6e1c1cfdc7961d3e3d74
Anjila Tripathi <anjila.tripathi@gmail.com>
2024-01-04 09:57:41 +0545

    Database Connectivity

--- JDBCExample/src/jdbcexample/JDBCExample.java
+++ JDBCExample/src/jdbcexample/JDBCExample.java
@@ -1,1 +1,13 @@
-Class.forName("com.mysql.cj.jdbc.Drivers");
+
+public class JDBCExample {
+
+    public static void main(String[] args)  throws Exception
+    {
+
+        Class.forName("com.mysql.cj.jdbc.Drivers"); //load and register Driver
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost=3306", "root", "");
+        Statement st = conn.getStatement();
+        Resultset rs = st.executeUpdate();
+        conn.close;
+    }
+}
